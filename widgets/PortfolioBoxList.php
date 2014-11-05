ffdf570da3a0182d05da2852cffe7a7bf18b9341
Anderson Danilo <contato@andersondanilo.com>
2014-11-04 22:56:27 -0200

    localized pages correction

--- widgets/PortfolioBoxList.php
+++ widgets/PortfolioBoxList.php
@@ -11,18 +11,7 @@ class PortfolioBoxList {
     public function register(array $options)
     {
         $category = PageCategory::where('type', '=', PageCategory::PORTFOLIO)->first();
-        if(moduleEnabled('i18n'))
-        {
-            $pages = $category->pages()->where(function($query) {
-                $query->where('language', '=', Config::get('app.locale'))
-                    ->orWhere('language', '=', '')
-                    ->orWhereNull('language');
-            })->get();
-        }
-        else
-        {
-            $pages = $category->pages;
-        }
+        $pages = $category->localizedPages()->get();
         return View::make(
             'portfolio::widgets.portfolioBoxList.list',
             compact('pages', 'options')
